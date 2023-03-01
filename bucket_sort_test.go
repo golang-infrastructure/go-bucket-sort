95776b2b5ccd6920193d30e81c05c9f6c0d54384
CC11001100 <CC11001100@qq.com>
2023-03-02 01:51:03 +0800

    feat: bucket排序支持任意类型的切片

--- bucket_sort_test.go
+++ bucket_sort_test.go
@@ -1,6 +1,8 @@
 package bucket_sort
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
 )
@@ -18,3 +20,32 @@ func TestSort(t *testing.T) {
 	t.Log(err)
 	t.Log(slice)
 }
+
+// ------------------------------------------------ ---------------------------------------------------------------------
+
+type user struct {
+	name string
+	age  int
+}
+
+func TestSortByFunc(t *testing.T) {
+	userSlice := make([]*user, 1000)
+	for i := 0; i < 1000; i++ {
+		userSlice[i] = &user{
+			name: fmt.Sprintf("name-%d", i),
+			age:  rand.Intn(99) + 1,
+		}
+	}
+
+	err := SortByFunc(userSlice, func(index int, value *user) int {
+		return value.age
+	})
+	assert.Nil(t, err)
+
+	for _, user := range userSlice {
+		t.Log(fmt.Sprintf("age: %d, name: %s\n", user.age, user.name))
+	}
+
+}
+
+// ------------------------------------------------ ---------------------------------------------------------------------
